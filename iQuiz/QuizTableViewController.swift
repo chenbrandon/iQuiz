087c71cd27d7e31e93736c60a322e60d3e76443d
Brandon Chen <chenb777@uw.edu>
2017-05-01 19:44:32 -0700

    settings button working, static cells not showing

--- iQuiz/QuizTableViewController.swift
+++ iQuiz/QuizTableViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class QuizTableViewController: UITableViewController {
 
+    @IBAction func settingsPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        self.present(alert, animated: true)
+        print("self present done")
+        NSLog("test")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
