6fd4b0e3d0eb9d0c7399a9605629689dba5d060e
Brandon Chen <chenb777@uw.edu>
2017-05-11 21:41:22 -0700

    storing and reading data, pesistence working

--- iQuiz/QuizTableViewController.swift
+++ iQuiz/QuizTableViewController.swift
@@ -37,6 +37,11 @@ class QuizTableViewController: UITableViewController {
                     print("Data is empty")
                     return
                 }
+                self.quiz = self.quiz2
+                self.descriptions = self.descriptions2
+                self.questions = self.questions2
+                self.answers = self.answers2
+                self.correctAnswers = self.correctAnswers2
                 let json = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [Dictionary<String, AnyObject>]
                 for i in json! {
                     let thisSubject = i["title"]
@@ -72,14 +77,27 @@ class QuizTableViewController: UITableViewController {
     }
     
     func refresh() {
-        DispatchQueue.main.asyncAfter(deadline: (DispatchTime.now() + 1)) {
+        DispatchQueue.main.asyncAfter(deadline: (DispatchTime.now() + 5)) {
             print("calling reload")
+            UserDefaults.standard.setValue(self.quiz, forKey: "subjects")
+            UserDefaults.standard.setValue(self.descriptions, forKey: "descriptions")
+            UserDefaults.standard.setValue(self.questions, forKey: "questions")
+            UserDefaults.standard.setValue(self.answers, forKey: "answers")
+            UserDefaults.standard.setValue(self.correctAnswers, forKey: "correctAnswers")
+            UserDefaults.standard.synchronize()
             self.tableView.reloadData()
+
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.quiz = UserDefaults.standard.value(forKey: "subjects") as! [String]
+        //self.descriptions = UserDefaults.standard.value(forKey: "descriptions") as! [String]
+        //self.questions = UserDefaults.standard.value(forKey: "questions") as! [[String]]
+        //self.answers = UserDefaults.standard.value(forKey: "answers") as! [[[String]]]
+        //self.correctAnswers = UserDefaults.standard.value(forKey: "correctAnswers") as! [[Int]]
+
         getJSON(self.url)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -158,45 +176,32 @@ class QuizTableViewController: UITableViewController {
         //cell.icon.image = UIImage(named: subject)
         return cell
     }
-    var quiz: [String] = ["Mathematics", "Marvel Super Heroes", "Science!","Luck"]
-    var descriptions: [String] = ["Test your math skills",
-                                  "Test your knowledge of Marvel",
-                                  "Test your understanding of the world",
-                                  "Test your Luck"]
-    var questions : [[String]] = [
-        ["Is 2 + 2 = 22 ?", "what is 5! ?"],
-        ["Who is iron man"],
-        ["What is water"],
-        ["Pick an option", "Pick another option", "Pick a final option"]
-    ]
-    var answers : [[[String]]] = [
-        [["no","yes","It's a trick question","Hillary's Emails"], ["when you shout 5", "1", "120", "0"]],
-        [["Tony Hawk", "Tony Stark", "Ronald McDonald", "Kaiba"]],
-        [["H20","Click here!","Yugi","C3H8 Propane"]],
-        [["1", "2", "3", "4"], ["1","2","3","4"],["1","2","3","4"]]
-    ]
+    var quiz: [String] = UserDefaults.standard.value(forKey: "subjects") as! [String]
+    var descriptions: [String] = UserDefaults.standard.value(forKey: "descriptions") as! [String]
+    var questions : [[String]] = UserDefaults.standard.value(forKey: "questions") as! [[String]]
+    var answers : [[[String]]] = UserDefaults.standard.value(forKey: "answers") as! [[[String]]]
     
-    var correctAnswers: [[Int]] = [[1,3], [2], [1], [3, 3, 4]]
-    /*
-    var quiz: [String] = ["Mathematics", "Marvel Super Heroes", "Science!","Luck"]
-    var descriptions: [String] = ["Test your math skills",
+    var correctAnswers: [[Int]] = UserDefaults.standard.value(forKey: "correctAnswers") as! [[Int]]
+    
+    var quiz2: [String] = ["My Math ", "My Marvel", "My Science","My Luck"]
+    var descriptions2: [String] = ["Test your math skills",
                                   "Test your knowledge of Marvel",
                                   "Test your understanding of the world",
                                   "Test your Luck"]
-    var questions : [[String]] = [
+    var questions2 : [[String]] = [
         ["Is 2 + 2 = 22 ?", "what is 5! ?"],
         ["Who is iron man"],
         ["What is water"],
         ["Pick an option", "Pick another option", "Pick a final option"]
     ]
-    var answers : [[[String]]] = [
+    var answers2 : [[[String]]] = [
         [["no","yes","It's a trick question","Hillary's Emails"], ["when you shout 5", "1", "120", "0"]],
         [["Tony Hawk", "Tony Stark", "Ronald McDonald", "Kaiba"]],
         [["H20","Click here!","Yugi","C3H8 Propane"]],
         [["1", "2", "3", "4"], ["1","2","3","4"],["1","2","3","4"]]
     ]
-    var correctAnswers: [[Int]] = [[1,3], [2], [1], [3, 3, 4]]
-    */
+    var correctAnswers2: [[Int]] = [[1,3], [2], [1], [3, 3, 4]]
+    
     
     func getSubject(_ n: Int) -> String {
         return quiz[n]
---
+++ iQuiz/UserDefaultsManager.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaultsManager.swift
+//  iQuiz
+//
+//  Created by Brandon Chen on 5/11/17.
+//  Copyright © 2017 Brandon Chen. All rights reserved.
+//
+
+import Foundation
